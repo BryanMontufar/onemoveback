06d01bdb887371bd8431b4c416f09ec2a6b28807
BryanMontufar <briiianmh@gmail.com>
2025-03-10 18:35:54 -0500

    Agregado formulario de Compra en ModalComponent

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\IsUserAuth;
 use App\Http\Controllers\Api\autosController;
 use App\Http\Controllers\Api\CompraController;
 
 Route::get('/autos', [autosController::class,'index']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/autos/{id}', [autosController::class, 'show']);
 
@@ -17,3 +19,12 @@ Route::put('/autos/{id}', [autosController::class, 'update']);
 Route::delete('/autos/{id}', [autosController::class, 'delete']);
 
 Route::post('/compras', [CompraController::class, 'store']);
+
+//PRIVATE ROUTES
+Route::middleware([IsUserAuth::class])->group(function(){
+   
+    Route::controller(AuthController::class)->group(function(){
+        Route::post('logout', 'logout');
+        Route::get('me', 'getUser');
+    });
+});
